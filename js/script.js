bdcd98607f8ab610b153956a6e5a4d611d66303a
yamanoha-hab <r12m097@yahoo.co.jp>
2021-11-14 19:41:27 +0900

    googleフォーム設定

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 'use strict';
 
 $(function() {
+  // メニューバー
+  $(".hamburger").click(function() {
+    $(this).toggleClass('hamburger__active');
+  });
+
   // スムーススクロール
   $('a[href^="#contact"]').click(function() {
     var target = $($(this).attr("href")).offset().top; //href属性の取得
@@ -11,18 +16,54 @@ $(function() {
     return false;
   });
 
-
   // アコーディオン
   $('.accordion__ttl').click(function() {
-    // if(!$(this).hasClass("accordion__body--active")) {
-      $(this).next().slideToggle();      
-      
-      return false;
+  // if(!$(this).hasClass("accordion__body--active")) {
+    $(this).next().slideToggle();      
+    
+    return false;
     // }
   });
 
-  // メニューバー
-  $(".hamburger").click(function() {
-    $(this).toggleClass('hamburger__active');
+  // フォームのprivacyCheckチェックさせる。
+  const $submitBtn = $('#js-submit')
+  $('#form input,#form textarea').on('change', function () {
+    if (
+      // 指定したinputが空なのかチェック
+      $('#form input[type="text"]').val() !== "" &&
+      $('#form input[type="email"]').val() !== "" &&
+      $('#form input[type="checkbox"]').val() !== "" &&
+      //privacyCheckにチェック入っているか確認
+      $('#form #privacyCheck').prop('checked') === true
+    ) {
+      // 空欄ではないならdisabledをはずす。(""が空の部分)
+      $submitBtn.prop('disabled', false);
+      
+    } else {
+      // disabled付く(送信できない)
+      $submitBtn.prop('disabled', true);
+    }
+  });
+
+  // 完了メッセージ 送信
+  $('#form').submit(function (event) {
+    var formData = $('#form').serialize();
+    $.ajax({
+      url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLScYsvRFEyMDy6oXsaGvthp2ZMfN-WZp6JUu7sYLzoPNIHk1JQ/formResponse",
+      data: formData,
+      type: "POST",
+      dataType: "xml",
+      statusCode: {
+        0: function () {
+          $(".end-message").slideDown();
+          $(".submit-btn").fadeOut();
+          //window.location.href = "thanks.html";
+        },
+        200: function () {
+          $(".false-message").slideDown();
+        }
+      }
+    });
+    event.preventDefault();
   });
 });
